5db595285bdac0ab7de020d9f1f329f34dca936a
Aram <aramnr@yahoo.com>
2017-11-22 13:31:29 +0100

    get request in js fixed

--- src/main/java/group1/toa/controller/ToaController.java
+++ src/main/java/group1/toa/controller/ToaController.java
@@ -1,18 +1,29 @@
 package group1.toa.controller;
 
+import group1.toa.domain.Toilet;
 import group1.toa.repository.ToaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class ToaController {
 
     @Autowired
     private ToaRepository toaRep;
+
+    @GetMapping("/toilets")
+    @ResponseBody
+    public List<Toilet> scriptReq() {
+        List<Toilet> toilets = toaRep.getAllToilets();
+        return toilets;
+    }
 
     @GetMapping ("/")
     public ModelAndView listToilets(){
