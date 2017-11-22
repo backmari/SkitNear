9142cf762bb3d66ccdcbd03d84cd65d5bc8b76a7
Rikard <rikard.weijmer@gmail.com>
2017-11-22 11:00:40 +0100

    ADD markergeneration logic

--- src/main/resources/static/mapBehaviour.js
+++ src/main/resources/static/mapBehaviour.js
@@ -1,12 +1,23 @@
-initmap = function(){
-    console.log("Tjena");
-    var uluru = {lat: -25.363, lng: 131.044};
+function initMap() {
+    var zoom = 10;
+
+
+    var toilets = [
+        {latitude: 59.32544622, longitud: 18.07585976 },
+        {latitude: 59.3136511, longitud: 18.0589745 },
+        {latitude: 59.3164926, longitud: 18.0835784 },
+        {latitude: 59.31573162, longitud: 18.08713373 },
+        {latitude: 59.31172369, longitud: 18.09105638 }
+    ];
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
-    });
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
-};
+        zoom: zoom,
+        center: {lat: toilets[1].latitude, lng: toilets[1].longitud}});
+
+    for(var i = 0; i<toilets.length; i++){
+        new google.maps.Marker({
+            position: {lat: toilets[i].latitude, lng: toilets[i].longitud},
+            map: map
+        });
+    }
+
+}
